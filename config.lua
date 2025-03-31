1a11e1c1fc83d16826ff2e029198e91c7f904926
PIN COBRA <118466716+PCOBRA@users.noreply.github.com>
2025-03-31 07:15:16 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,25 +3,28 @@ Config = {}
 Config.CraftingRecipes = {
     {
         result = 'bread',
+        label = 'Bánh mì',
         ingredients = {
-            ['wheat'] = 2,
-            ['water'] = 1
+            ['gotap'] = { amount = 2, label = 'Gạo tấp' },
+            ['water'] = { amount = 1, label = 'Nước' }
         },
-        time = 5000 -- Thời gian điều chế cho 1 vật phẩm
+        time = 5000
     },
     {
         result = 'weapon_pistol',
+        label = 'Súng ngắn',
         ingredients = {
-            ['metalscrap'] = 10,
-            ['gunpowder'] = 5
+            ['metalscrap'] = { amount = 10, label = 'Phế liệu kim loại' },
+            ['gunpowder'] = { amount = 5, label = 'Thuốc súng' }
         },
-        time = 10000 -- Thời gian điều chế cho 1 vật phẩm
+        time = 10000
     }
 }
 
-Config.CraftingLocation = vector3(100.0, 200.0, 30.0)
-Config.RequiredCops = 2
+Config.CraftingLocation = vector3(2588.76, 4849.24, 34.96)
+Config.RequiredCops = 0
 Config.PoliceJob = 'police'
 Config.MaxDistance = 3.0
-Config.MaxQuantity = 10 -- Giới hạn tối đa số lượng vật phẩm mỗi lần
-Config.MaxCraftingPlayers = 3 -- Giới hạn tối đa số người điều chế cùng lúc
+Config.MaxQuantity = 10
+Config.MaxCraftingPlayers = 3
+Config.WebhookURL = 'YOUR_DISCORD_WEBHOOK_URL_HERE' -- Thay bằng URL webhook Discord của bạn
