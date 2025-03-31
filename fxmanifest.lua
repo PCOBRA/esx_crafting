cdfd90e81dcd48484614a53258eec099e030972e
PIN COBRA <118466716+PCOBRA@users.noreply.github.com>
2025-03-31 07:16:03 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes' -- Thêm dòng này để bật Lua 5.4
 
 author 'Pin Cobra'
 description 'Script điều chế vật phẩm cho ESX Legacy với ox_inventory và ox_lib'
-version '1.0.0'
+version '1.1.0'
 
 shared_scripts {
     '@es_extended/imports.lua',
